0cbcd4e20e077babb726b7fe032abee9982ff004
deepblue <deepblue@inha.ac.kr>
2025-04-16 15:38:43 +0900

    v1.6 kiosk file IO

--- kiosk.py
+++ kiosk.py
@@ -1,6 +1,13 @@
 # 16번 커피 키오스크 예제를 리스트 하나로 통합
 
 def select_menu(i):
+    """
+    1) 메뉴 선택 시 해당 메뉴 중간 디스플레이
+    2) 선택한 메뉴 수량 업데이트
+    3) 소계 기능
+    :param i: 선택한 메뉴 - 1
+    :return: 없음
+    """
     menus[i][1] = menus[i][1] + 1
     print(f"{menus[i][0]} {menus[i][1]}잔 주문...")
     subtotal = 0
@@ -10,6 +17,10 @@ def select_menu(i):
 
 
 def print_receipt():
+    """
+    영수증 출력 기능
+    :return: 없음
+    """
     print("=" * 38)
     total_price = 0
     for j in range(len(menus)):
@@ -18,6 +29,20 @@ def print_receipt():
             total_price = total_price + (menus[j][1] * menus[j][2])  # 가격 리스트에서 가격 추출해서 합산
     print(f"총 금액은 {total_price}원 입니다.")
 
+
+def get_ticket_number():
+    """
+    번호표 기능 (파일 입출력)
+    :return: 번호
+    """
+    with open("ticket.txt", "r") as fp:
+        # print(fp.read())
+        number = int(fp.read()) + 1
+        # print(number)
+    with open("ticket.txt", "w") as fp:
+        fp.write(str(number))
+
+    return number
 
 menus = [["아이스 아메리카노", 0, 2000], ["카페 라떼", 0, 2500], ["유자차", 0, 2400], ["자바칩 프라푸치노", 0, 7000]]  # [[메뉴, 수량, 단가], ...]
 #prices = [2000, 2500, 2400, 7000]
@@ -38,3 +63,4 @@ while True:
 
 
 print_receipt()
+print(f"번호표 : {get_ticket_number()}")
